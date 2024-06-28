6758de165fb0dbe82801630cd80607adcf01ad14
polinnab <p.tsunenko94@gmail.com>
2024-06-28 15:17:07 +0300

    test localhost page Next logo exists

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3001/");
+
+    cy.get('[data-cy="main-logo"]').should("exist");
+  });
+});
