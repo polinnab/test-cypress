28d51469926628e41a369207da3613c0388deb25
polinnab <p.tsunenko94@gmail.com>
2025-02-27 10:36:19 +0200

    routing e2e tests

--- src/app/page-content/DialogPage/index.tsx
+++ src/app/page-content/DialogPage/index.tsx
@@ -1,5 +1,5 @@
 import {  Stack, Typography } from "@mui/material"
 
 export const DialogPage = () => {
-    return(<Stack my={4} mx={3} justifyContent='center'><Typography variant="h1" textAlign='center'>Page Two</Typography></Stack>)
+    return(<Stack my={4} mx={3} justifyContent='center'><Typography variant="h1" textAlign='center'>Dialog page</Typography></Stack>)
 }
