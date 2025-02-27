1f04f125c1e4302bc606124a2bb000320424f96c
polinnab <p.tsunenko94@gmail.com>
2025-02-27 10:06:41 +0200

    added second page and links between pages

---
+++ src/app/dialog-page/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+import { DialogPage } from "../page-content/DialogPage";
+
+export default function Page() {
+  return (
+    <main >
+      <Link href='/'>Go to Home page</Link>
+      <DialogPage />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import { TodoList } from "./page-content/TodoList";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main >
+      <Link href='/dialog-page'>Go to Dialog page</Link>
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <TodoList />
+      </div>
     </main>
   );
 }
