4fb29fd062db6c95cf28042cc020b7cf83c66ccf
polinnab <p.tsunenko94@gmail.com>
2025-02-27 11:50:51 +0200

    added dialog mock

---
+++ src/app/page-content/DialogPage/Dialog/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import DialogMui from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export const Dialog = ({isOpen,close}: {isOpen: boolean, close: () => void}) => {
+    return(<DialogMui
+        fullWidth
+        maxWidth='xl'
+        open={isOpen}
+        onClose={close}
+      >
+        <DialogTitle>Test form dialog</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            You can fill in whatever you want under the form rules. This dialog only for cypress component testing.
+          </DialogContentText>
+          <Box
+            noValidate
+            component="form"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              m: 'auto',
+              width: 'fit-content',
+            }}
+          >
+            Content
+            
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={close}>Close</Button>
+        </DialogActions>
+      </DialogMui>)
+}
--- src/app/page-content/DialogPage/index.tsx
+++ src/app/page-content/DialogPage/index.tsx
@@ -1,5 +1,23 @@
-import {  Stack, Typography } from "@mui/material"
+"use client";
+import {  Box, Button, Stack, Typography } from "@mui/material"
+import { useState } from "react";
+import { Dialog } from "./Dialog";
 
 export const DialogPage = () => {
-    return(<Stack my={4} mx={3} justifyContent='center'><Typography variant="h1" textAlign='center'>Dialog page</Typography></Stack>)
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClickOpen = () =>  setIsOpen(true);
+    
+    const handleClose = () => setIsOpen(false);
+
+    return(
+    <Stack my={4} mx={3} justifyContent='center'>
+    <Typography variant="h1" textAlign='center'>Dialog page</Typography>
+    <Box margin='64px auto'>
+     <Button variant="outlined" onClick={handleClickOpen}>
+        Open dialog
+      </Button>
+      </Box>
+      <Dialog isOpen={isOpen} close={handleClose}/>
+    </Stack>)
 }
