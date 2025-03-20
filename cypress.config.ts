2feece4b34fa138f443842f3f3a4bce243682522
polinnab <p.tsunenko94@gmail.com>
2025-03-20 10:39:04 +0200

    updated cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3001/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
+	defaultBrowser: 'chrome',
+	e2e: {
+		baseUrl: 'http://localhost:3001/',
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+	component: {
+		viewportWidth: 1400,
+		viewportHeight: 1080,
+		devServer: {
+			framework: 'next',
+			bundler: 'webpack',
+		},
+	},
 });
